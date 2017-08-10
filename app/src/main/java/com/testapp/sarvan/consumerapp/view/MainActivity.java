fc579821f27c0f045361099b77dc44c0de8f0993
Saravan Sekar Thillai <sarvan2shekar@gmail.com>
2017-08-10 15:57:35 +0530

    Created a consumer app for consuming the data from Content provider

--- app/src/main/java/com/testapp/sarvan/consumerapp/view/MainActivity.java
+++ app/src/main/java/com/testapp/sarvan/consumerapp/view/MainActivity.java
@@ -8,7 +8,6 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
